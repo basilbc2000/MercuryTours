1179a43d70997f91c0df4ae037d92b9bdd624728
basilbc2000 <45725909+basilbc2000@users.noreply.github.com>
2018-12-12 23:46:10 +0530

    Verifying maven profile for parallel and single run builds

---
+++ TestAutomationProject/src/test/java/runner/SingleRunner.java
@@ -0,0 +1,20 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = {"stepDefinitions"},        
+        features = {"src/test/resources/features/ApplicationPages.feature"},
+        plugin = {"html:target/cucumber-reports", "json:target/cucumber-reports/Cucumber.json"}
+)
+
+public class SingleRunner {
+
+	public SingleRunner() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
