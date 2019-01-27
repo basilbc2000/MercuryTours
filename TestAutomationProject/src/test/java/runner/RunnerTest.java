e71e7bcfab9e1f621d8ecd5bbfb359ccb3e10333
basilbc2000 <45725909+basilbc2000@users.noreply.github.com>
2019-01-28 00:32:02 +0530

    testing pipeline

--- TestAutomationProject/src/test/java/runner/RunnerTest.java
+++ TestAutomationProject/src/test/java/runner/RunnerTest.java
@@ -6,9 +6,9 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		//features = {"src/test/resources/application/features/ApplicationPages.feature"},
+		features = {"src/test/resources/application/features/ApplicationPages.feature"},
 		//features = {"src/test/resources/application/features/UserRegistration.feature"},
-		features = {"src/test/resources/application/features/"},
+		//features = {"src/test/resources/application/features/"},
         glue = {"mercurysd"},                
         plugin = {"html:target/cucumber-reports", "json:target/cucumber-reports/Cucumber.json"}
 )
