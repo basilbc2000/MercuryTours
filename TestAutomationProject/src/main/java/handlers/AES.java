23d9ca21983449d504e43fc199dcc6b908e40a0c
basilbc2000 <45725909+basilbc2000@users.noreply.github.com>
2019-03-17 22:42:25 +0530

    AES Decryption in Java

--- TestAutomationProject/src/main/java/handlers/AES.java
+++ TestAutomationProject/src/main/java/handlers/AES.java
@@ -1,84 +1,156 @@
 package handlers;
 
-//https://aesencryption.net/
-	
+import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.util.Arrays;
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
 
 public class AES {
 
-	private static SecretKeySpec secretKey;
-	private static byte[] key;
-	private static String decryptedMsg;
-	private static String encryptedMsg;
+	private static final Charset ASCII = Charset.forName("ASCII");
+	private static final int INDEX_KEY = 0;
+	private static final int INDEX_IV = 1;
+	private static final int ITERATIONS = 1;
+	private static final int SALT_OFFSET = 8;
+	private static final int SALT_SIZE = 8;
+	private static final int CIPHERTEXT_OFFSET = SALT_OFFSET + SALT_SIZE;
+	private static final int KEY_SIZE_BITS = 256;
 
-	public static void setKey(String myKey) {
-		MessageDigest sha = null;
-		try {
-			key = myKey.getBytes("UTF-8");
-			//System.out.println(key.length);
-			sha = MessageDigest.getInstance("SHA-1");
-			key = sha.digest(key);
-			key = Arrays.copyOf(key, 16); // use only first 128 bit
-			//System.out.println(key.length);
-			//System.out.println(new String(key, "UTF-8"));
-			secretKey = new SecretKeySpec(key, "AES");
-		} catch (Exception e) {e.printStackTrace();}
+	/**
+	 * Thanks go to Ola Bini for releasing this source on his blog. The source was
+	 * obtained from <a href="http://olabini.com/blog/tag/evp_bytestokey/">here</a>
+	 * 
+	 */
+
+	public static byte[][] EVP_BytesToKey(int key_len, int iv_len, MessageDigest md, byte[] salt, byte[] data,
+			int count) {
+
+		byte[][] both = new byte[2][];
+		byte[] key = new byte[key_len];
+		int key_ix = 0;
+		byte[] iv = new byte[iv_len];
+		int iv_ix = 0;
+		both[0] = key;
+		both[1] = iv;
+		byte[] md_buf = null;
+		int nkey = key_len;
+		int niv = iv_len;
+		int i = 0;
+		if (data == null) {
+			return both;
+		}
+		int addmd = 0;
+		for (;;) {
+			md.reset();
+			if (addmd++ > 0) {
+				md.update(md_buf);
+			}
+			md.update(data);
+			if (null != salt) {
+				md.update(salt, 0, 8);
+			}
+			md_buf = md.digest();
+			for (i = 1; i < count; i++) {
+				md.reset();
+				md.update(md_buf);
+				md_buf = md.digest();
+			}
+			i = 0;
+			if (nkey > 0) {
+				for (;;) {
+					if (nkey == 0)
+						break;
+					if (i == md_buf.length)
+						break;
+					key[key_ix++] = md_buf[i];
+					nkey--;
+					i++;
+				}
+			}
+			if (niv > 0 && i != md_buf.length) {
+				for (;;) {
+					if (niv == 0)
+						break;
+					if (i == md_buf.length)
+						break;
+					iv[iv_ix++] = md_buf[i];
+					niv--;
+					i++;
+				}
+			}
+			if (nkey == 0 && niv == 0) {
+				break;
+			}
+		}
+		for (i = 0; i < md_buf.length; i++) {
+			md_buf[i] = 0;
+		}
+		return both;
 	}
 
-	public static String getDecryptedMsg() {
+	//Create Key & IV
+	public static byte[][] getKeyIV(byte[] headerSaltAndCipherText, Cipher aesCBC, String password) {		
+		byte[] salt = Arrays.copyOfRange(headerSaltAndCipherText, SALT_OFFSET, SALT_OFFSET + SALT_SIZE);
+		byte[][] keyAndIV=null;
+		try {
+			MessageDigest md5 = MessageDigest.getInstance("MD5");
+			keyAndIV = EVP_BytesToKey(KEY_SIZE_BITS / Byte.SIZE, aesCBC.getBlockSize(), md5, salt,
+					password.getBytes(ASCII), ITERATIONS);
+		} catch (Exception e) {e.printStackTrace();}
+		
+		return keyAndIV;
+	}
+
+	// https://stackoverflow.com/questions/11783062/how-to-decrypt-file-in-java-encrypted-with-openssl-command-using-aes
+	public static String decrypt(String encryptedMsg, String password) {
+
+		String decryptedMsg =null;		
+		byte[] headerSaltAndCipherText = Base64.decodeBase64(encryptedMsg);
+		byte[] encrypted = Arrays.copyOfRange(headerSaltAndCipherText, CIPHERTEXT_OFFSET, headerSaltAndCipherText.length);
+		try {
+			Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			final byte[][] keyAndIV = getKeyIV(headerSaltAndCipherText, aesCBC, password);
+			SecretKeySpec key = new SecretKeySpec(keyAndIV[INDEX_KEY], "AES");
+			IvParameterSpec iv = new IvParameterSpec(keyAndIV[INDEX_IV]);
+			aesCBC.init(Cipher.DECRYPT_MODE, key, iv);
+			byte[] decrypted = aesCBC.doFinal(encrypted);
+			decryptedMsg = new String(decrypted, ASCII);
+		} catch (Exception e) {e.printStackTrace();}
+		
+		return decryptedMsg;
+	}
+	
+	public static String encrypt(String msg, String password) {
+
+		String decryptedMsg =null;		
+		byte[] headerSaltAndCipherText = Base64.decodeBase64(msg);
+		byte[] encrypted = Arrays.copyOfRange(headerSaltAndCipherText, CIPHERTEXT_OFFSET, headerSaltAndCipherText.length);
+		try {
+			Cipher aesCBC = Cipher.getInstance("AES/CBC/PKCS5Padding");
+			final byte[][] keyAndIV = getKeyIV(headerSaltAndCipherText, aesCBC, password);
+			SecretKeySpec key = new SecretKeySpec(keyAndIV[INDEX_KEY], "AES");
+			IvParameterSpec iv = new IvParameterSpec(keyAndIV[INDEX_IV]);
+			aesCBC.init(Cipher.ENCRYPT_MODE, key, iv);
+			//Base64.encodeBase64String(aesCBC.doFinal(msg.getBytes("UTF-8")));
+			//byte[] decrypted = aesCBC.doFinal(encrypted);
+			//decryptedMsg = new String(decrypted, ASCII);
+			decryptedMsg = Base64.encodeBase64String(aesCBC.doFinal(msg.getBytes("UTF-8")));
+		} catch (Exception e) {e.printStackTrace();}
+		
 		return decryptedMsg;
 	}
 
-	public static void setDecryptedMsg(String message) {
-		AES.decryptedMsg = message;
-	}
+	public static void main(String[] args) {
 
-	public static String getEncryptedMsg() {
-		return encryptedMsg;
-	}
-
-	public static void setEncryptedMsg(String message) {
-		AES.encryptedMsg = message;
-	}
-
-	public static String encrypt(String message) {
-		try {
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-			setEncryptedMsg(Base64.encodeBase64String(cipher.doFinal(message.getBytes("UTF-8"))));
-		} catch (Exception e) {System.out.println("Error while encrypting: " + e.toString());}
-		return null;
-	}
-
-	public static String decrypt(String message) {
-		try {
-			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-			setDecryptedMsg(new String(cipher.doFinal(Base64.decodeBase64(message))));
-		} catch (Exception e) {System.out.println("Error while decrypting: " + e.toString());}
-		return null;
-	}
-
-	public static void main(String args[]) {
-		String message = "sample";
-		final String key= "test";
+		String msg = "the decrypted message is this";
+		String password = "pass";
 		
-		AES.setKey(key);
-		AES.encrypt(message.trim());
-
-		System.out.println("String to Encrypt: " + message);
-		System.out.println("Encrypted: " + AES.getEncryptedMsg());
-
-		message = AES.getEncryptedMsg();
-		AES.decrypt(message.trim());
-
-		System.out.println("String To Decrypt : " + message);
-		System.out.println("Decrypted : " + AES.getDecryptedMsg());
-
+		System.out.println(encrypt(msg, password));
+		
+		String encryptedMsg = "U2FsdGVkX190A5FsNTanwTKBdex29SpnH4zWkZN+Ld+MmbJgK4BH1whGIRRSpOJT";
+		System.out.println(decrypt(encryptedMsg, password));
 	}
-
 }
