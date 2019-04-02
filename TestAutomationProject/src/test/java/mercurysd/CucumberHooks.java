895a1c56862fb0820dab2c33d37524f3028045ab
basilbc2000 <45725909+basilbc2000@users.noreply.github.com>
2019-04-02 18:08:21 +0530

    Testing SQLite

--- TestAutomationProject/src/test/java/mercurysd/CucumberHooks.java
+++ TestAutomationProject/src/test/java/mercurysd/CucumberHooks.java
@@ -36,7 +36,14 @@ public class CucumberHooks {
 	
 	public CucumberHooks(TestContext context) {
 		tc = context;
+		//clearRunDataTbl();
 	}
+	
+	/*public void clearRunDataTbl () {
+		Connection con = FileHandlers.handle().sqlLiteDb().openRunDataDB();
+		FileHandlers.handle().sqlLiteDb().clearRunData(con);		
+		FileHandlers.handle().sqlLiteDb().closeRunDataDB(con);
+	}*/
 
 	@Before
 	public void BeforeSteps(Scenario scenario) {
--- TestAutomationProject/src/test/java/runner/RunnerTest.java
+++ TestAutomationProject/src/test/java/runner/RunnerTest.java
@@ -18,5 +18,5 @@ import managers.FileHandlers;
 )
 
 public class RunnerTest {
-	
+
 }
